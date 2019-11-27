7da80aada3e9282568680c10c4eba85390f10f64
Nikolay Kostyurin <jilizart@gmail.com>
2019-11-27 23:53:58 +0200

    chore: fonts css

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,19 +14,47 @@ import withWeather from '../hooks/withWeather';
 import memoize from 'fast-memoize';
 
 const styles = css`
+	/* latin */
+	@font-face {
+	  font-family: 'PT Root UI';
+	  font-style: normal;
+	  font-weight: normal;
+	  src:
+		local('PT Root UI'),
+		local('PTRootUI'),
+		url(assets/fonts/PTRootUIRegular.woff2) format('woff2'),
+		url(assets/fonts/PTRootUIRegular.woff) format('woff');
+	  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2212, U+2215;
+	  font-display: swap;
+	}
+	/* latin */
+	@font-face {
+	  font-family: 'Merriweather';
+	  font-style: normal;
+	  font-weight: normal;
+	  src:
+		local('Merriweather'),
+		url(assets/fonts/Merriweather-Regular.woff2) format('woff2'),
+		url(assets/fonts/Merriweather-Regular.woff) format('woff');
+	  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2212, U+2215;
+	  font-display: swap;
+	}
+
 	body {
+		min-height: 100%;
+		margin: 0;
 		background: ${colorGray300};
 	}
 `;
 
 const renderAutocomplete = ({ index, data, onSetIndex }) =>
-	<WeatherAutocomleteList onItemClick={onSetIndex} data={data} activeIndex={index}/>;
+	<WeatherAutocomleteList onItemClick={onSetIndex} data={data} activeIndex={index} />
 
 export default () => {
 	const { addByName, items, removeById } = withWeather();
 
 	const onSearchChange = memoize(({ item, index }) => {
-		addByName(item.title)
+		addByName(item.title);
 	});
 
 	const onItemRemove = memoize((item) => removeById(item));
