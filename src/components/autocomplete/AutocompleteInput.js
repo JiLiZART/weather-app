b5f90396464d89705046afc5ca17954743349cfb
Nikolay Kostyurin <jilizart@gmail.com>
2019-11-29 00:13:00 +0200

    feat: add error and loading state to autocomplete input

--- src/components/autocomplete/AutocompleteInput.js
+++ src/components/autocomplete/AutocompleteInput.js
@@ -2,10 +2,17 @@ import React from 'react';
 import Autocomplete from './Autocomplete';
 import Input from '../input/Input';
 
-const AutocompleteInput = ({ renderAutocomplete, onAutocompleteChange, data, ...props }) => (
-	<Autocomplete renderAutocomplete={renderAutocomplete} onAutocompleteChange={onAutocompleteChange} data={data}>
+const AutocompleteInput = ({ renderAutocomplete, onAutocompleteChange, data, error, value, isLoading, ...props }) => (
+	<Autocomplete
+		renderAutocomplete={renderAutocomplete}
+		onAutocompleteChange={onAutocompleteChange}
+		data={data}
+		error={error}
+		value={value}
+		isLoading={isLoading}
+	>
 		{(inputProps) => (
-			<Input {...props} {...inputProps} />
+			<Input {...props} {...inputProps} value={value} />
 		)}
 	</Autocomplete>
 );
