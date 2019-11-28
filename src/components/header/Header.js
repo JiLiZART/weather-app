b5e89cb30a6092ee63633ab78f2d6205f1702352
Nikolay Kostyurin <jilizart@gmail.com>
2019-11-29 00:13:57 +0200

    feat: header style by design

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { colorInputGray300 } from '../variables';
 
 const Centered = styled.div`
+	position: relative;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	margin-bottom: 32px;
+	
+	:before,
+	:after {
+		position: absolute;
+		top: 50%;
+		display: block;
+		content: '';
+		width: calc(50% - 40px);
+		height: 1px;
+		transform: translateY(50%);
+		background: ${colorInputGray300};
+	}
+	
+	:before {
+		left: 0;
+	}
+	
+	:after {
+		right: 0;
+	}
 `;
 
 const Header = ({ children }) => (
