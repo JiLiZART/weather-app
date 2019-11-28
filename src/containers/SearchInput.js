ce1bc093910508149b3e258182ca8f4279bfa27e
Nikolay Kostyurin <jilizart@gmail.com>
2019-11-29 00:17:10 +0200

    feat: add onAutocompleteChange prop to SearchInput

--- src/containers/SearchInput.js
+++ src/containers/SearchInput.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from '../components/button/Button';
 import Icon from '../components/icon/Icon';
-import { compose, withHandlers, withStateHandlers } from 'recompose';
+import compose from 'recompose/compose';
+import withHandlers from 'recompose/withHandlers';
+import withStateHandlers from 'recompose/withStateHandlers';
 import AutocompleteInput from '../components/autocomplete/AutocompleteInput';
 
-
 const SearchButton = () => (
-	<Button state="primary"><Icon.Search/></Button>
+	<Button state="primary"><Icon.Search /></Button>
 );
 
 const SearchInput = compose(
@@ -21,14 +22,18 @@ const SearchInput = compose(
 	withHandlers({
 		onChange: props => e => {
 			props.setValue(e.target.value);
+
+			if (props.onChange) {
+				props.onChange(e.target.value);
+			}
 		},
 		onAutocompleteChange: props => ({ index }) => {
 			const item = props.data[index];
 
 			props.setValue(item.title);
 
-			if (props.onChange) {
-				props.onChange({ item, index });
+			if (props.onAutocompleteChange) {
+				props.onAutocompleteChange({ item, index });
 			}
 		}
 	})
