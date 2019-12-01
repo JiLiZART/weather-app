e6b548068aa4d1d1dca0d60b99fd14cbb0d7a26f
Nikolay Kostyurin <jilizart@gmail.com>
2019-12-02 00:41:42 +0200

    feat: add bold style for PT Root UI font

--- src/components/globalStyles.js
+++ src/components/globalStyles.js
@@ -15,6 +15,18 @@ export default css`
 	  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2212, U+2215;
 	  font-display: swap;
 	}
+	@font-face {
+	  font-family: 'PT Root UI';
+	  font-style: normal;
+	  font-weight: bold;
+	  src:
+		local('PT Root UI'),
+		local('PTRootUI'),
+		url(assets/fonts/PTRootUIBold.woff2) format('woff2'),
+		url(assets/fonts/PTRootUIBold.woff) format('woff');
+	  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2212, U+2215;
+	  font-display: swap;
+	}
 	/* latin */
 	@font-face {
 	  font-family: 'Merriweather';
